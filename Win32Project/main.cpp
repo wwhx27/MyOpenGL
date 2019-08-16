763a131726dce03557e3e87875737d45ed959aa0
wwhx27 <wwhx27@yahoo.com>
2019-08-16 21:48:56 +0800

    construct window and register window

--- Win32Project/main.cpp
+++ Win32Project/main.cpp
@@ -2,5 +2,26 @@
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	//构造窗口
+	WNDCLASSEX wndclass;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.cbWndExtra = 0;
+	wndclass.hbrBackground = NULL;
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIcon = NULL;
+	wndclass.hIconSm = NULL;
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = NULL;
+	wndclass.lpszClassName = L"GLWindow";
+	wndclass.lpszMenuName = NULL;
+	wndclass.style = CS_VREDRAW | CS_HREDRAW;
+	//注册窗口
+	ATOM atom = RegisterClassEx(&wndclass);
+	if (!atom)
+	{
+		MessageBox(NULL, L"Notice", L"Error", MB_OK);
+		return 0;
+	}
 	return 0;
 }
