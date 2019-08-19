2f371a3ec2c06e800792948ef3b3b81710dfcdeb
wwhx27 <wwhx27@yahoo.com>
2019-08-19 18:15:15 +0800

    compile shader

--- Win32Project/utils.cpp
+++ Win32Project/utils.cpp
@@ -1,1 +1,19 @@
 #include "utils.h"
+GLuint CompileShader(GLenum shaderType, const char* shaderCode)
+{
+	GLuint shader = glCreateShader(shaderType);//根据shader的种类，来创建一个shader的对象
+	glShaderSource(shader, 1, &shaderCode, nullptr);//shader对应的源码
+	glCompileShader(shader);//用显卡驱动编译shader
+	GLint compileResult = GL_TRUE;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compileResult);//查看shader的编译结果
+	if (compileResult == GL_FALSE)
+	{
+		char szLog[1024] = { 0 };
+		GLsizei logLen = 0;
+		glGetShaderInfoLog(shader, 1024, &logLen, szLog);//获取错误日志
+		printf("Compile Shader fail error log: %s \nshader code:\n%s\n", szLog, shaderCode);
+		glDeleteShader(shader);//编辑失败后删除shader
+		shader = 0;
+	}
+	return shader;
+}
--- Win32Project/utils.h
+++ Win32Project/utils.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "ggl.h"
 unsigned char* LoadFileContent(const char* path, int& filesize);
+GLuint CompileShader(GLenum shaderType, const char* shaderCode);
