4f73463eea67a8add8982c4c58be681c6fc969ab
wwhx27 <wwhx27@yahoo.com>
2019-08-19 23:34:12 +0800

    set value to shader (attribute)

--- Win32Project/scene.cpp
+++ Win32Project/scene.cpp
@@ -37,9 +37,25 @@ void SetViewPortSize(float width, float height)
 }
 void Draw()
 {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);//设置擦除背景的颜色
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(program);
 	glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
 	glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(viewMatrix));
 	glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projectMatrix));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);//设置当前的数据集,把vbo设置为当前要绘制图形的数据集，就是程序或者叫shader在绘制的时候去vbo取数据
+	glEnableVertexAttribArray(positionLocation);//启用属性组（AttribArray）中positionLocation这个插槽
+	//第一个参数：插槽的位置；第二个参数：这个插槽中数据有几个分量；第三个参数：表示每个分量的类型
+	//第四个参数：需不需要归一化，假如传入颜色值时255，255，255，他不是浮点数，如果归一化就把255映射为1.0
+	//第五个参数：紧挨着的两个点相距多远
+	//第六个参数：从vbo的哪个位置开始取值
+	//设置这些参数的目的是告诉gpu，如何去遍历vbo这个内存块
+	glVertexAttribPointer(positionLocation, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);
+	//第一个参数：表示绘制三角形，每三点个绘制一个面；第二个参数：从第几个点开始绘制；第三个参数：要绘制多少个点
+	//这句话在执行的时候，在gpu端，他会遍历vbo中的数据，并分别传入三个不同的shader，这三个shader分别拿到了不同的点，
+	//然后他们的viewMatrix，modelmatrix，projectMatrix是一样的，之后这三个点通过shader会在gpu上不同的三个核心（core）上同时执行
+	//这个函数的目的：告诉gpu如何把vbo中的数据分发到各个不同的shader，让他们去执行
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glUseProgram(0);
 }
