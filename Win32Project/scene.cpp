4306f33bed17fc2db0f26cc1af4450d0a5c61f14
wwhx27 <wwhx27@yahoo.com>
2019-08-19 19:38:01 +0800

    send shader to gpu

--- Win32Project/scene.cpp
+++ Win32Project/scene.cpp
@@ -1,6 +1,8 @@
 #include "scene.h"
 #include "ggl.h"
+#include "utils.h"
 GLuint vbo;
+GLuint program;
 void Init()
 {
 	float data[] = {
@@ -12,6 +14,16 @@ void Init()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);//把vbo设置为当前的vbo
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, data, GL_STATIC_DRAW);//操作当前这个GL_ARRAY_BUFFER，在gpu开辟sizeof(float)*12的空间，将data从cpu发送到gpu，把数据data放到显卡上后就不会再修改他了
 	glBindBuffer(GL_ARRAY_BUFFER, 0);//设置当前的GL_ARRAY_BUFFER为零，表示把cpu端的数据删掉
+	int fileSize = 0;
+	unsigned char* shaderCode = LoadFileContent("Res/test.vs", fileSize);
+	GLuint vsShader = CompileShader(GL_VERTEX_SHADER, (char*)shaderCode);
+	delete shaderCode;
+	shaderCode = LoadFileContent("Res/test.fs", fileSize);
+	GLuint fsShader = CompileShader(GL_FRAGMENT_SHADER, (char*)shaderCode);
+	delete shaderCode;
+	program = CreateProgram(vsShader, fsShader);
+	glDeleteShader(vsShader);
+	glDeleteShader(fsShader);
 }
 void SetViewPortSize(float width, float height)
 {
