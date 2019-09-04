680092032ce34f549371e680505a41be12538ece
wwhx27 <wwhx27@yahoo.com>
2019-09-04 12:22:46 +0800

    decode picture(.bmp)

--- Win32Project/utils.cpp
+++ Win32Project/utils.cpp
@@ -38,3 +38,22 @@ GLuint CreateProgram(GLuint vsShader, GLuint fsShader)
 	}
 	return program;
 }
+
+unsigned char* DecodeBMP(unsigned char* bmpFileData, int& width, int& height)
+{
+	if (0x4D42 == *((unsigned short*)bmpFileData))
+	{
+		int pixelDataOffset = *((int*)(bmpFileData + 10));
+		width = *((int*)(bmpFileData + 18));
+		height = *((int*)(bmpFileData + 22));
+		unsigned char* pixelData = bmpFileData + pixelDataOffset;
+		for (int i = 0; i < width*height * 3; i += 3)
+		{
+			unsigned char temp = pixelData[i];
+			pixelData[i] = pixelData[i + 2];
+			pixelData[i + 2] = temp;
+		}
+		return pixelData;
+	}
+	return nullptr;
+}
--- Win32Project/utils.h
+++ Win32Project/utils.h
@@ -4,3 +4,4 @@ unsigned char* LoadFileContent(const char* path, int& filesize);
 GLuint CompileShader(GLenum shaderType, const char* shaderCode);
 GLuint CreateProgram(GLuint vsShader, GLuint fsShader);
 float GetFrameTime();
+unsigned char* DecodeBMP(unsigned char* bmpFileData, int& width, int& height);
